e8bd579f8ba50d18ba7cf6209c76a5e36c76edfd
Luis Fernandes <zipleen@gmail.com>
2018-02-26 08:28:56 +0000

    attempt to fix this crash  that keeps popping up and I have no idea why - _currentlyPlayingEpgStore becomes null on the next line, which must mean the variable must have changed mid build

--- tvheadend-ios-lib/TVHChannelStoreAbstract.m
+++ tvheadend-ios-lib/TVHChannelStoreAbstract.m
@@ -25,18 +25,28 @@
 @implementation TVHChannelStoreAbstract
 
 - (id <TVHEpgStore>)currentlyPlayingEpgStore {
+    
     if (!_currentlyPlayingEpgStore){
-        _currentlyPlayingEpgStore = [self.tvhServer createEpgStoreWithName:@"CurrentlyPlaying"];
-        if (_currentlyPlayingEpgStore != nil) {
-            [_currentlyPlayingEpgStore setDelegate:self];
-            // we can't have the object register the notification, because every channel has one epgStore - that would make every epgStore object update itself!!
-            [[NSNotificationCenter defaultCenter] addObserver:_currentlyPlayingEpgStore
-                                                     selector:@selector(appWillEnterForeground:)
-                                                         name:UIApplicationWillEnterForegroundNotification
-                                                       object:nil];
+        if ([[NSThread currentThread] isMainThread]) {
+            [self buildCurrentlyPlayingEpgStore];
+        } else {
+            dispatch_sync(dispatch_get_main_queue(), ^{
+                [self buildCurrentlyPlayingEpgStore];
+            });
         }
     }
     return _currentlyPlayingEpgStore;
+}
+
+- (void)buildCurrentlyPlayingEpgStore {
+    _currentlyPlayingEpgStore = [self.tvhServer createEpgStoreWithName:@"CurrentlyPlaying"];
+    [_currentlyPlayingEpgStore setDelegate:self];
+    // we can't have the object register the notification, because every channel has one epgStore - that would make every epgStore object update itself!!
+    [[NSNotificationCenter defaultCenter] addObserver:_currentlyPlayingEpgStore
+                                             selector:@selector(appWillEnterForeground:)
+                                                 name:UIApplicationWillEnterForegroundNotification
+                                               object:nil];
+    
 }
 
 - (id)initWithTvhServer:(TVHServer*)tvhServer {
